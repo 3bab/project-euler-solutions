170bcbdcb1748d87892e6a98fd0121f509f853d9
Gintas <gintas.palionis@gmail.com>
2020-04-17 17:01:01 +0200

    5

--- 5.kts
+++ 5.kts
@@ -5,13 +5,16 @@ What is the smallest positive number that is evenly divisible by all of the numb
  */
 
 fun smallestDivisible(): Long {
-
+    for (i in 10 until Long.MAX_VALUE) {
+        if (divide(i, 2)) return i
+    }
+    return 0
 }
 
-fun divide(div: Long) {
-
+fun divide(n: Long, div: Long): Boolean {
+    if (div == 21L) return true
+    if (n.rem(div) == 0L) return divide(n, div + 1)
+    return false
 }
-
-
 
 println(smallestDivisible())
