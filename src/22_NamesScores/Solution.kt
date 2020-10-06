cf43714376ccffd94c9554a411c98b2c2141305f
Gintas <gintas.palionis@gmail.com>
2020-10-06 11:55:18 +0200

    22

--- src/22_NamesScores/Solution.kt
+++ src/22_NamesScores/Solution.kt
@@ -1,5 +1,11 @@
 package `22_NamesScores`
 
+import java.io.File
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.stream.Collectors
+import kotlin.test.assertEquals
+
+
 /*
 Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names,
 begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value
@@ -10,5 +16,31 @@ the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
 
 What is the total of all the name scores in the file?
  */
+
+
+fun main() {
+    val names = File("/Users/gintas.palionis/IdeaProjects/project-euler-solutions/src/22_NamesScores/p022_names.txt")
+            .readLines()
+            .get(0)
+            .split(",")
+            .stream()
+            .map { i -> i.replace("\"", "") }
+            .collect(Collectors.toList())
+
+    val scoreMap: HashMap<String, Int> = calculateScores(names)
+    assertEquals(49714, scoreMap.get("COLIN"))
+    print(scoreMap.map { e -> e.value }.sum())
+}
+
+fun calculateScores(names: List<String>): HashMap<String, Int> {
+    val sortedNames = names.stream().sorted().collect(Collectors.toList())
+    val scoreMap: HashMap<String, Int> = HashMap()
+    val index = AtomicInteger(0)
+    sortedNames.stream().forEach { n ->
+        val sum: Int = n.toCharArray().map { ch -> ch.toInt() - 64 }.sum()
+        scoreMap.put(n, sum * index.incrementAndGet())
+    }
+    return scoreMap
+}
 
 
